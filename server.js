d0ce55ff290bc8c1d67cd85b2c4d71840185fa62
Edgar Mendez <edgar.ym26@gmail.com>
2022-06-23 13:11:36 -0400

    added to config connection , req seqlize and dotenv

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const path = require('path');
 
-const PORT = process.env.PORT || 3001;
+
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'));
+
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 
 app.get("/", async (req,res) => {
 
