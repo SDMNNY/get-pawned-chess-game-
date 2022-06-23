8cbfba7850db2103ed807faa4c2ea07fc8c523e6
sdmnny <sdmnny@gmail.com>
2022-06-23 14:34:29 -0400

    created inital controller and user route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,10 +5,23 @@ const withAuth = require('../utils/auth');
 
 
 router.get('/', withAuth, async (req, res) => {
-    try { 
-
+    try {
+      const userData = await User.findAll({
+        attributes: { exclude: ['password'] },
+        order: [['name',]],
+      });
+  
+      const users = userData.map((project) => project.get({ plain: true }));
+  
+      res.render('homepage', {
+        users,
+        logged_in: req.session.logged_in,
+      });
+    } catch (err) {
+      res.status(500).json(err);
     }
-})
+});
+
 
 
 
