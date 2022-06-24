0d65cf1697f6b7c8c50595c41c4f44ff49234af8
Edgar Mendez <edgar.ym26@gmail.com>
2022-06-24 11:04:31 -0400

    forced commit to merge new

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -22,42 +22,9 @@ router.get('/', async (req, res) => {
       games.get({ plain: true })
     );
 
-<<<<<<< HEAD
-
-
-router.get('/login',(req,res)=>{res.render("login")})
-router.get('/logout',(req,res)=>{res.render("logout")})
-
-// Get one 
-router.get('/user/:id', async (req, res) => {
-  if (!req.session.loggedIn) { 
-    res.redirect('/login');
-  } else { 
-    try { 
-      const userData = await User.findByPk(req.params.id, {
-        include : [
-          {
-            model: User,
-            attributes: [
-              "username",
-              "email",
-              "password",
-            ],
-          },
-        ],
-  
-      });
-      const user = userData.get({ plain: true });
-      res.render('user', {user, loggedIn: req.session.loggedIn});
-    } catch (err) {
-      console.log(err); 
-      res.status(500).json(err);
-    }
-=======
     res.render('homepage', { games, loggedIn: req.session.loggedIn });
   } catch (err) {
     res.status(500).json(err);
->>>>>>> 817c269e9d7f1e2ef83af4a53897bdcad6658ad0
   }
 });
 
