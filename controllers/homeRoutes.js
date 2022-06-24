309fba4502e2c0b3c0e4e2d93a27e050aebbc333
Edgar Mendez <edgar.ym26@gmail.com>
2022-06-24 10:50:22 -0400

    seeding seems to work

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,28 +1,22 @@
 const router = require('express').Router();
-const { Game, User } = require('../models');
+const { Move, User, Game } = require('../models');
 // custom middleware 
 const withAuth = require('../utils/auth');
 
 
 // Get All
-router.get('/', withAuth, async (req, res) => {
-
-    try {
-      const userData = await User.findAll({
-        attributes: { exclude: ['password'] },
-        order: [['name',]],
-      });
-  
-      const users = userData.map((project) => project.get({ plain: true }));
-  
-      res.render('homepage', {
-        users,
-        logged_in: req.session.logged_in,
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
+router.get('/', async (req, res) => {
+  try {
+    res.render('homepage', {
+      galleries,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
+
 
 
 router.get('/login',(req,res)=>{res.render("login")})
