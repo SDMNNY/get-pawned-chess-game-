42bc198d62b22e5649b581c9c9909c669a8deff4
Edgar Mendez <edgar.ym26@gmail.com>
2022-06-24 12:30:15 -0400

    challenge button works on main page , trying to get users to show up

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -69,6 +69,15 @@ router.post('/', async (req, res) => {
       res.status(404).end();
     }
   });
+
+  router.get('/challengepage', async (req, res) => {
+    const allUsers = await User.findAll()
+    const users = allUsers.map((user) =>
+    user.get({plain:true}))
+    res.render('challengepage', {
+      users
+    });
+  });
   
 
 module.exports = router;
