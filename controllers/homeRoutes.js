a5427cb09a13bff14928229db8896395c62892c9
sdmnny <sdmnny@gmail.com>
2022-06-24 11:00:27 -0400

    edited homeroutes in controller file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,76 +4,148 @@ const { Game, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 
-// Get All
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const gameData = await Product.findAll({
+      include: [
+        {
+          model: User,
+          attributes: { exclude: ['password'] },
+        },
+        {
+          model: Game,
+        },
+      ],
+    });
 
-    try {
-      const userData = await User.findAll({
-        attributes: { exclude: ['password'] },
-        order: [['name',]],
-      });
-  
-      const users = userData.map((user) => user.get({ plain: true }));
-  
-      res.render('homepage', {
-        users,
-        logged_in: req.session.logged_in,
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-});
+    const games = gameData.map((game) =>
+      games.get({ plain: true })
+    );
 
-
-router.get('/login',(req,res)=>{res.render("login")})
-router.get('/logout',(req,res)=>{res.render("logout")})
-
-// Get one 
-router.get('/user/:id', async (req, res) => {
-  if (!req.session.loggedIn) { 
-    res.redirect('/login');
-  } else { 
-    try { 
-      const userData = await User.findByPk(req.params.id, {
-        include : [
-          {
-            model: User,
-            attributes: [
-              "username",
-              "email",
-              "password",
-            ],
-          },
-        ],
-  
-      });
-      const user = userData.get({ plain: true });
-      res.render('user', {user, loggedIn: req.session.loggedIn});
-    } catch (err) {
-      console.log(err); 
-      res.status(500).json(err);
-    }
+    res.render('homepage', { games, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    res.status(500).json(err);
   }
-  
 });
 
+router.get('/games/:id', async (req, res) => {
+  try {
+    const gameData = await Game.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: { exclude: ['password'] },
+        },
+        {
+          model: Game,
+        },
+      ],
+    });
 
+    const singleGame = productData.get({ plain: true });
 
+    res.render('singleGame', {
+      singleGame,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-
-
-
-router.get("/login", (req, res) => {
+router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect("/");
+    res.redirect('/');
     return;
   }
 
-  res.render("login");
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('signup');
 });
 
 
-
-
-
 module.exports = router;
+
+
+
+// // Get All
+// router.get('/', withAuth, async (req, res) => {
+
+//     try {
+//       const userData = await User.findAll({
+//         attributes: { exclude: ['password'] },
+//         order: [['name',]],
+//       });
+  
+//       const users = userData.map((user) => user.get({ plain: true }));
+  
+//       res.render('homepage', {
+//         users,
+//         logged_in: req.session.logged_in,
+//       });
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+// });
+
+
+// router.get('/login',(req,res)=>{res.render("login")})
+// router.get('/logout',(req,res)=>{res.render("logout")})
+
+// // Get one 
+// router.get('/user/:id', async (req, res) => {
+//   if (!req.session.loggedIn) { 
+//     res.redirect('/login');
+//   } else { 
+//     try { 
+//       const userData = await User.findByPk(req.params.id, {
+//         include : [
+//           {
+//             model: User,
+//             attributes: [
+//               "username",
+//               "email",
+//               "password",
+//             ],
+//           },
+//         ],
+  
+//       });
+//       const user = userData.get({ plain: true });
+//       res.render('user', {user, loggedIn: req.session.loggedIn});
+//     } catch (err) {
+//       console.log(err); 
+//       res.status(500).json(err);
+//     }
+//   }
+  
+// });
+
+
+
+
+
+
+
+// router.get("/login", (req, res) => {
+//   if (req.session.loggedIn) {
+//     res.redirect("/");
+//     return;
+//   }
+
+//   res.render("login");
+// });
+
+
+
+
+
+// module.exports = router;
+
